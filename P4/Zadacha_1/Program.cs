4fc84736a0fa21cc1cdd6700a5574ecc26d8fd7c
Zotov Maksim <prolabor@bk.ru>
2023-03-08 11:34:48 +0300

    Praktika

---
+++ P4/Zadacha_1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.Clear();
+int numA = GetNumUser("Введите число А: ", "Ошибка!!! Введите верное число");
+int numB = GetNumUser("Введите число B: ", "Ошибка!!! Введите верное число");
+int degreeOf = GetDegreeOf(numA, numB);
+Console.WriteLine($"Число {numA} в натуральной степени {numB} -> {degreeOf}");
+
+
+int GetNumUser(string text, string textError) {
+    while(true) {
+        Console.Write(text);
+        bool flag = int.TryParse(Console.ReadLine(), out int number);
+        if(flag)
+            return number;
+        Console.WriteLine(textError);
+    }
+}
+
+int GetDegreeOf(int number_A, int number_B) {
+    int result = 1;
+    if (number_B < 0)
+         number_B = -1 * number_B;
+    for (int i = 1; i <= number_B; i++) {
+        result = result * number_A;
+    }
+    numB = number_B;
+    return result;
+}
---
+++ P4/Zadacha_2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.Clear();
+int num = GetInputNum("Введите число: ", "Ошибка!!! Повторите ввод числа");
+int result = SumNumber(num);
+Console.WriteLine("Сумма цифр в числе: " + result);
+
+
+int GetInputNum(string text, string textError) {
+     while(true) {
+        Console.Write(text);
+        bool flag = int.TryParse(Console.ReadLine(), out int number);
+        if(flag)
+            return number;
+        Console.WriteLine(textError);
+    }
+}
+
+int SumNumber(int num) {
+    int counter = Convert.ToString(num).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++){
+      advance = num - num % 10;
+      result = result + (num - advance);
+      num = num / 10;
+    }
+   return result;
+}
