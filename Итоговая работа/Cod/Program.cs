5426ce708b6d977cc7d7272a2ad4ef365a5e419d
Zotov Maksim <prolabor@bk.ru>
2023-05-04 14:56:37 +0300

    Praktika

---
+++ Итоговая работа/Cod/Program.cs
@@ -0,0 +1,36 @@
+﻿//Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+//длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+//либо задать на старте выполнения алгоритма. 
+Console.Clear();
+string[] array1 = new string[5] {"computer science", "23", ":-)", "world", "1"};
+string[] array2 = new string[5];
+PrintArray(array1, "Ваш заданный массив:");
+Result(array1, array2);
+PrintArray(array2, "Ваш новый массив:");
+
+void Result(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+
+void PrintArray(string[] array, string text)
+{
+    Console.WriteLine(text);
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+
